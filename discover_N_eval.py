c7bc974e2cabc4a69a7041049479934cffe41593
Gavriel Habib <gavri1991@gmail.com>
2018-08-05 23:05:14 +0300

    more accurate version of discover_N_eval

--- discover_N_eval.py
+++ discover_N_eval.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 FLAGS = tf.app.flags.FLAGS
 
-def evaluate(seq_length, N_values, charmap):
+def evaluate(seq_length, N, charmap):
 
     lines, _, _ = model_and_data_serialization.load_dataset(seq_length=seq_length, b_charmap=False, b_inv_charmap=False,
                                                             n_examples=FLAGS.MAX_N_EXAMPLES, dataset='heldout')
@@ -35,53 +35,33 @@ def evaluate(seq_length, N_values, charmap):
         load_from_curr_session=True)  # global param, always load from curr session after finishing the first seq
 
     cnt = 0
-    Linf_norm_values = np.zeros([int(len(N_values)/2), 1])
+    Linf_norm_values = []
     N_graph_values = []
 
-    for N in N_values:
-        print("N = %d\n" % (N))
-        # train_pred -> run session
-        train_pred_all = np.zeros([N, BATCH_SIZE, seq_length, train_pred.shape[2]],
-                                dtype=np.float32)
-        BPC_list = []
+    print("N = %d\n" % (N))
+    # train_pred -> run session
+    train_pred_all = np.zeros([N, BATCH_SIZE, seq_length, train_pred.shape[2]],
+                            dtype=np.float32)
 
-        #for start_line in range(0, len(lines) - BATCH_SIZE + 1, BATCH_SIZE):
-        for start_line in range(1):
-            _data = np.array([[charmap[c] for c in l] for l in lines[start_line:start_line + BATCH_SIZE]])
+    for start_line in range(1):
+        _data = np.array([[charmap[c] for c in l] for l in lines[start_line:start_line + BATCH_SIZE]])
 
-            # rand N noise vectors and for each one - calculate train_pred.
-            for i in range(N):
-                train_pred_i = sess.run(train_pred_for_eval, feed_dict={real_inputs_discrete: _data})
-                train_pred_all[i,:,:,:] = train_pred_i
+        # rand N noise vectors and for each one - calculate train_pred.
+        for i in range(N):
+            train_pred_i = sess.run(train_pred_for_eval, feed_dict={real_inputs_discrete: _data})
+            train_pred_all[i,:,:,:] = train_pred_i
 
-            # take average on each time step (first dimension)
-            train_pred_average = np.mean(train_pred_all, axis=0)
-            if N % 100 == 0:
-                N_graph_values.append(N)
-                train_pred_average_diff = train_pred_average - train_pred_average_prev
-                Linf_norm_diff = np.linalg.norm(train_pred_average_diff, axis=2, ord=np.inf)
-                Linf_norm_values[cnt] = np.mean(Linf_norm_diff)
-                cnt += 1
-            train_pred_average_prev = train_pred_average
+    N_run = range(10, N+10, 10)
+    train_pred_average_prev = np.mean(train_pred_all[0:N_run[0]], axis=0)
 
-            # compute BPC (char-based perplexity)
-            train_pred_average_2d = train_pred_average.reshape([train_pred_average.shape[0]*train_pred_average.shape[1],
-                                                                train_pred_average.shape[2]])
-            real_data = _data.reshape([_data.shape[0]*_data.shape[1]])
-
-            BPC = 0
-
-            epsilon = 1e-20
-            for i in range(real_data.shape[0]):
-                BPC -= np.log2(train_pred_average_2d[i,real_data[i]]+epsilon)
-
-            BPC /= real_data.shape[0]
-            # print("BPC of start_line %d = %.2f\n" % (start_line, BPC))
-
-            BPC_list.append(BPC)
-
-    BPC_final = np.mean(BPC_list)
-    print("BPC_final = %.2f\n" % (BPC_final))
+    for N_curr in N_run[1:]:
+        N_graph_values.append(N_curr)
+        train_pred_average = np.mean(train_pred_all[0:N_curr], axis=0)
+        train_pred_average_diff = train_pred_average - train_pred_average_prev
+        Linf_norm_diff = np.linalg.norm(train_pred_average_diff, axis=2, ord=np.inf)
+        Linf_norm_values.append(np.mean(Linf_norm_diff))
+        cnt += 1
+        train_pred_average_prev = train_pred_average
 
     plt.figure()
     plt.plot(N_graph_values, Linf_norm_values)
@@ -97,12 +77,5 @@ def get_internal_checkpoint_dir(seq_length):
 
 _, charmap, _ = model_and_data_serialization.load_dataset(seq_length=32, b_lines=False)
 eval_seq_length = 7
-N_values_1 = range(90, 5090, 100)
-N_values_2 = range(100, 5100, 100)
-N_values = []
-
-for i in range(len(N_values_1)):
-    N_values.append(N_values_1[i])
-    N_values.append(N_values_2[i])
-
-evaluate(eval_seq_length, N_values, charmap)
+N_max = 3000
+evaluate(eval_seq_length, N_max, charmap)
